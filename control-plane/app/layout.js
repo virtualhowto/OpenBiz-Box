36a373e86b19f2125814da6ac8428408d36bb2cc
Scott McGregor <scottm@hyperscott.com>
2026-08-19 23:47:49 +1000

    Add control plane layout

---
+++ control-plane/app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'OpenBiz Box',description:'Run your business. Own your stack. Talk to your business.'};
+export default function Layout({children}){return <html lang="en"><body>{children}</body></html>}
